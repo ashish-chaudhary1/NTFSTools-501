08c8d721f5f0c3ebed2ab8d734988c572f7ac25e
ashis <ashis@Blaze>
2025-08-13 00:43:09 +0530

    commit form dev1 for pohoepay1

--- GitProj01/src/com/nt/main/PhonePayPayment.java
+++ GitProj01/src/com/nt/main/PhonePayPayment.java
@@ -5,4 +5,8 @@ public class PhonePayPayment {
 	public String PhonePay(String user) {
 		return user+" is using PhonePe pay hello";
 	}
+	
+	public String PhonePay1(String user) {
+		return user+" is using PhonePe pay hello";
+	}
 }
